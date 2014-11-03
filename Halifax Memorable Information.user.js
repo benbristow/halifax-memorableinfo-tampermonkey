51d63f95e2952b9cb9f288e5e11eedd999839dcd
Ben Bristow <me@benbristow.co.uk>
2014-11-03 22:36:54 +0000

    First commit

---
+++ Halifax Memorable Information.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Halifax Memorable Information
+// @namespace    https://github.com/benbristow/halifax-memorableinfo-tampermonkey
+// @version      0.1
+// @description  Automatically inputs 'memorable information' on Halifax's horrible online banking site
+// @author       You
+// @match        https://secure.halifax-online.co.uk/personal/a/logon/entermemorableinformation.jsp
+// @grant        none
+// ==/UserScript==
+
+var memorableInfo = "changeme"
+document.getElementsByTagName("SELECT")[0].value = "&nbsp;" + memorableInfo[$("#frmentermemorableinformation1 > fieldset > div > div > div:nth-child(1) > label").text().replace(/[^\/\d]/g,'') - 1];
+document.getElementsByTagName("SELECT")[1].value = "&nbsp;" + memorableInfo[$("#frmentermemorableinformation1 > fieldset > div > div > div:nth-child(2) > label").text().replace(/[^\/\d]/g,'') - 1];
+document.getElementsByTagName("SELECT")[2].value = "&nbsp;" + memorableInfo[$("#frmentermemorableinformation1 > fieldset > div > div > div:nth-child(3) > label").text().replace(/[^\/\d]/g,'') - 1];
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-halifax-memorableinfo-tampermonkey
-==================================
+Halifax Memorable Information Tampermonkey Script
+======================================================
 
 Tampermonkey Script that autofill's Halifax Online Banking's annoying 'Memorable Information' dropdown boxes.
+
+Probably works on Firefox's GreaseMonkey addon as well.
