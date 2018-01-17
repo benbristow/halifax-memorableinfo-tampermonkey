359766f02df1e2343dd15f27e99f8fba6ac091ca
Ben Bristow <me@benbristow.co.uk>
2018-01-17 17:50:21 +0000

    Refactor, auto submit form

--- Halifax Memorable Information.user.js
+++ Halifax Memorable Information.user.js
@@ -2,12 +2,23 @@
 // @name         Halifax Memorable Information
 // @namespace    https://github.com/benbristow/halifax-memorableinfo-tampermonkey
 // @version      0.1
-// @description  Automatically inputs 'memorable information' on Halifax's horrible online banking site
+// @description  Automatically inputs 'memorable information' on Halifax's banking site
 // @author       Ben Bristow
 // @match        https://secure.halifax-online.co.uk/personal/a/logon/entermemorableinformation.jsp
 // ==/UserScript==
 
-var memorableInfo = "changeme"
-document.getElementsByTagName("SELECT")[0].value = "&nbsp;" + memorableInfo[$("#frmentermemorableinformation1 > fieldset > div > div > div:nth-child(1) > label").text().replace(/[^\/\d]/g,'') - 1];
-document.getElementsByTagName("SELECT")[1].value = "&nbsp;" + memorableInfo[$("#frmentermemorableinformation1 > fieldset > div > div > div:nth-child(2) > label").text().replace(/[^\/\d]/g,'') - 1];
-document.getElementsByTagName("SELECT")[2].value = "&nbsp;" + memorableInfo[$("#frmentermemorableinformation1 > fieldset > div > div > div:nth-child(3) > label").text().replace(/[^\/\d]/g,'') - 1];
+(function() {
+    var memorableInfo = 'changeme123';
+
+    var requiredFields = jQuery('label').map(function() {
+        var requiredIndex = parseInt(jQuery(this).text().trim().replace('Character ', '')) - 1;
+        return memorableInfo[requiredIndex];
+    });
+
+    for(var i = 0; i < 3; i++) {
+        jQuery('select').get(i).value = '&nbsp;' + requiredFields[i];
+    }
+
+    jQuery('.primaryAction > input').trigger('click');
+}());
+
